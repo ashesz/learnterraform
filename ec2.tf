245b6723946fbde9de96d7916933f48a6aebc29a
ashesz <74199576+ashesz@users.noreply.github.com>
2023-02-09 21:23:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "aws" {
   ami = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
 }
-output "myec2_IP" {
-  value = aws_instance.myec2.private_ip  
-}
+#output "myec2_IP" {
+#  value = aws_instance.myec2.private_ip  
+#}
