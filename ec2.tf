ce9c85f3f9f3907154398b1d79868709407f01f1
ashesz <74199576+ashesz@users.noreply.github.com>
2023-02-09 21:19:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-
-resource "aws_instance" "myec2" {
-  ami = "ami-0cca134ec43cf708f"
+resource "aws_instance" "aws" {
+  ami = "ami-01a4f99c4ac11b03c"
   instance_type = "t2.micro"
 }
 output "myec2_IP" {
